97811b3826935e4d94bd911dcd99e1804160890f
Ivan Kaziev <ivkaziev@yandex.ru>
2024-02-04 13:12:43 +0300

    Some fixes

--- graphs.py
+++ graphs.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import plotly.offline as offline
 
 
-async def load_graph() -> None:
+def load_graph() -> None:
     data = pd.read_excel('C:\\Users\\ivank\\PycharmProjects\\meteosite\\data\\AE33-S09-01249\\2023_07_AE33-S09-01249.xlsx')
     data['day'] = data['Datetime'].apply(lambda x: "-".join(x.split()[0].split('.')[::-1]) + " " + x.split()[1])
     data['day'] = pd.to_datetime(data['day'], format='%Y-%m-%d %H:%M')
